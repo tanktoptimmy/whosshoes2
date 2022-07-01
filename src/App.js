9d347a00752b33a31b5551d30d430e988ede81e0
Timothy Parry <tim.parry@skybettingandgaming.com>
2022-07-01 14:13:42 +0100

    shoes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { css, StyleSheet } from "aphrodite";
 
-function App() {
+const Boo = () => {
+
+  const [shoe, setShoe] = useState(0);
+  const [show, setShow] = useState(false);
+  const [reveal, setReveal] = useState(false);
+
+  const persons = ["Alex", "Layla", "Jess", "Rory", "Chris", "Andy", "Ashley", "Lisa", "Cy", "Sharyn", "Tim", "Jacob", "Adam"]
+  const person = persons[shoe]
+  const suffix = person === "Layla" ? "png" : "jpg";
+  const showing = `https://timparry.bitbucket.io/images/shoes/${person.toLowerCase()}.${suffix}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className={css(styles.main)}>
 
-export default App;
+    <div><h1>Who's Shoes Are Those?</h1>
+      <p>The Yello-Jello Shoe Guessing Game</p>
+    </div>
+    {!show && <button onClick={()=>setShow(true)}>START GUESSING</button>}
+
+    {show && <>
+      <div className={css(styles.pictureHolder)}>
+        <div className={css(styles.picture)}>
+          <img className={css(styles.shoePic)} src={showing} alt="shoe"/>
+          {reveal && <div className={css(styles.person)}>{person}</div>}
+        </div>
+      </div>
+      <div className={css(styles.buttonHolder)}>
+        <button  disabled={shoe === 0  ? "disabled" : false} onClick={()=>{
+          setShoe(shoe-1);
+          setReveal(false);
+        }}>Previous</button>
+        <button disabled={shoe < persons.length -1 ? false : "disabled"} onClick={()=>{
+          setShoe(shoe+1)
+          setReveal(false)
+        }}>Next</button>
+        <button onClick={()=>setReveal(true)}>Reveal</button>
+      </div>
+    </>
+    }
+  </div>
+)
+};
+ 
+export default Boo;
+
+const styles = StyleSheet.create({
+  main: {
+    fontFamily: "Garamond",
+    display: "flex",
+    flexDirection: "column",
+    height: "90vh",
+    justifyContent: "space-between",
+    textAlign: "center"
+  },
+  buttonHolder: {
+    marginTop: "21px"
+  },
+  pictureHolder: {
+    height: "800px",
+    width: "800px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "auto",
+    backgroundImage: "url('https://timparry.bitbucket.io/images/frames/frame0.png')",
+    backgroundSize: "contain",
+    backgroundRepeat:"no-repeat",
+    backgroundPosition: "center"
+  },
+  picture: {
+    height: "70%",
+    width: "40%",
+    border: "10px solid black",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative"
+  },
+  shoePic: {
+    maxWidth: "320px",
+    maxHeight: "400px"
+  },
+  person: {
+    position: "absolute",
+    bottom: "8px",
+    backgroundColor: "#ffdf00",
+    padding: "8px",
+    fontFamily: "Garamond"
+  }
+});
